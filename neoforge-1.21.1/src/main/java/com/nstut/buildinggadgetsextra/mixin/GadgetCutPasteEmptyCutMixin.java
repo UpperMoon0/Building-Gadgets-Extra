dcf5a42e43c93d78e14039127607ac9e26c54562
Vo Nguyen Minh Nhat <60876500+UpperMoon0@users.noreply.github.com>
2026-09-02 03:23:17 +0700

    fix(1.21.1): disable remapping for BG2 cut mixin

--- neoforge-1.21.1/src/main/java/com/nstut/buildinggadgetsextra/mixin/GadgetCutPasteEmptyCutMixin.java
+++ neoforge-1.21.1/src/main/java/com/nstut/buildinggadgetsextra/mixin/GadgetCutPasteEmptyCutMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(GadgetCutPaste.class)
+@Mixin(value = GadgetCutPaste.class, remap = false)
 public abstract class GadgetCutPasteEmptyCutMixin {
     @Inject(
             method = "cutAndStore",
