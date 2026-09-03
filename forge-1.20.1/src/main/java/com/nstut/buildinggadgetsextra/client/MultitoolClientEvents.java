6f5e32cc6362f52fcabd0b6715ee1f12344b6f9f
Vo Nguyen Minh Nhat <60876500+UpperMoon0@users.noreply.github.com>
2026-09-03 19:16:00 +0700

    Route 1.20.1 range hotkey through multitool packet

--- forge-1.20.1/src/main/java/com/nstut/buildinggadgetsextra/client/MultitoolClientEvents.java
+++ forge-1.20.1/src/main/java/com/nstut/buildinggadgetsextra/client/MultitoolClientEvents.java
@@ -2,13 +2,13 @@ package com.nstut.buildinggadgetsextra.client;
 
 import com.direwolf20.buildinggadgets2.client.KeyBindings;
 import com.direwolf20.buildinggadgets2.common.items.BaseGadget;
-import com.direwolf20.buildinggadgets2.common.network.PacketHandler;
-import com.direwolf20.buildinggadgets2.common.network.packets.PacketRangeChange;
 import com.direwolf20.buildinggadgets2.util.GadgetNBT;
 import com.nstut.buildinggadgetsextra.common.ExtraConstants;
 import com.nstut.buildinggadgetsextra.common.MultitoolMode;
 import com.nstut.buildinggadgetsextra.item.BuildersMultitool;
 import com.nstut.buildinggadgetsextra.item.MultitoolState;
+import com.nstut.buildinggadgetsextra.network.ExtraNetwork;
+import com.nstut.buildinggadgetsextra.network.MultitoolRangePacket;
 import com.nstut.buildinggadgetsextra.setup.ExtraConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.ItemStack;
@@ -40,7 +40,7 @@ public final class MultitoolClientEvents {
             int max = ExtraConfig.multitoolMaxRange();
             int oldRange = GadgetNBT.getToolRange(stack);
             int newRange = oldRange >= max ? 1 : oldRange + 1;
-            PacketHandler.sendToServer(new PacketRangeChange(newRange));
+            ExtraNetwork.sendToServer(new MultitoolRangePacket(newRange));
         }
     }
 }
