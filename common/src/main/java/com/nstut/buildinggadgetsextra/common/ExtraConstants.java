94f0481d3af890ed6ceb57dd697daa312328ac0c
Sunday Vo <60876500+UpperMoon0@users.noreply.github.com>
2026-09-10 19:47:36 +0700

    Add live mirror UI and status keys

--- common/src/main/java/com/nstut/buildinggadgetsextra/common/ExtraConstants.java
+++ common/src/main/java/com/nstut/buildinggadgetsextra/common/ExtraConstants.java
@@ -4,10 +4,16 @@ public final class ExtraConstants {
     public static final String MOD_ID = "buildinggadgetsextra";
     public static final String MIRROR_HORIZONTAL = MOD_ID + ".radialmenu.mirror_horizontal";
     public static final String MIRROR_VERTICAL = MOD_ID + ".radialmenu.mirror_vertical";
+    public static final String LIVE_MIRROR_HORIZONTAL = MOD_ID + ".radialmenu.live_mirror_horizontal";
+    public static final String LIVE_MIRROR_VERTICAL = MOD_ID + ".radialmenu.live_mirror_vertical";
     public static final String NO_TEMPLATE = MOD_ID + ".message.no_template";
     public static final String BUSY = MOD_ID + ".message.busy";
     public static final String MIRRORED_HORIZONTAL = MOD_ID + ".message.mirrored_horizontal";
     public static final String MIRRORED_VERTICAL = MOD_ID + ".message.mirrored_vertical";
+    public static final String LIVE_MIRROR_HORIZONTAL_ENABLED = MOD_ID + ".message.live_mirror_horizontal_enabled";
+    public static final String LIVE_MIRROR_HORIZONTAL_DISABLED = MOD_ID + ".message.live_mirror_horizontal_disabled";
+    public static final String LIVE_MIRROR_VERTICAL_ENABLED = MOD_ID + ".message.live_mirror_vertical_enabled";
+    public static final String LIVE_MIRROR_VERTICAL_DISABLED = MOD_ID + ".message.live_mirror_vertical_disabled";
     public static final String SAVE_STRUCTURE = MOD_ID + ".radialmenu.save_structure";
     public static final String LOAD_STRUCTURE = MOD_ID + ".radialmenu.load_structure";
     public static final String CUT_SELECTION = MOD_ID + ".radialmenu.cut_selection";
